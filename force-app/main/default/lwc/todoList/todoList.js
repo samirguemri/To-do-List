da737d3c718516c1ea77fe8a33069a29544f7cc7
Samir Guemri <samir.guemri@gmail.com>
2022-09-01 17:46:27 +0200

    fetshing tasks from Salesforce with Apex

--- force-app/main/default/lwc/todoList/todoList.js
+++ force-app/main/default/lwc/todoList/todoList.js
@@ -1,27 +1,45 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import fetshTasks from '@salesforce/apex/TodoListController.getTasks';
 
 export default class TodoList extends LightningElement {
 
-    lastId = 0;
-    newTask;
+    newTask = '';
+    @track tasks = [];
+    error;
 
-    @track
-    tasks = [];
+    @wire(fetshTasks)
+    wiredTasks(response) {
+        let data = response.data;
+        let error = response.error;
+
+        if (data) {
+            this.error = undefined;
+            data.forEach(task => {
+                this.tasks.push({
+                    Id: task.Id,
+                    Subject: task.Subject
+                });
+            });
+        } else if (error) {
+            this.error = error;
+            this.tasks = undefined;
+        }
+    }
 
     updateNewTask(event) {
         this.newTask = event.target.value;
     }
 
-    addNewTasktoList(event) {
-        this.tasks.unshift({
-            id: ++this.lastId,
-            task: this.newTask
+    addNewTasktoList() {
+        this.tasks.push({
+            Id: 11111,
+            Subject: this.newTask
         });
         this.newTask = '';
     }
 
     deleteTaskFromList(event) {
         const idToDelete = event.target.name;
-        this.tasks = this.tasks.filter(value => value.id !== idToDelete);
+        this.tasks = this.tasks.filter(task => task .Id !== idToDelete);
     }
 }
